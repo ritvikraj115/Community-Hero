73f59577c2bf4f374e9a54a7c61d3a003a61739b
ritvik-raj-singh <2023ugme011@nitjsr.ac.in>
2026-06-30 15:41:37 +0530

    added render file

--- community-hero-frontend/src/App.js
+++ community-hero-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import AdminSetup from './components/AdminSetup';
